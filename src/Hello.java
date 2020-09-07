6ca0f36fd05c27410b7336a07417f6c7b2872c02
Sara Sandager <sarasandager@MacBook-Pro-tilhrende-Sara.local>
2020-09-07 11:29:10 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    // psvm - public static void main(String[] args){}
+    public static void main(String[] args) {
+        // sout - System.out.println("text");
+        System.out.println("Hello World!");
+        System.out.println("Print text line 2");
+        System.out.println("Print text line 3");
+        System.out.println("Print text line 4");
+        System.out.println("Print text line 5");
+    }
+}
