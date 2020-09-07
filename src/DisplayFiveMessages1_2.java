4ee680ce812c82f4a6359907b0dc03e2af6cfb76
sarasandager <sara590x@edu.easj.dk>
2020-09-07 21:37:31 +0200

    Assignment 1.1 and 1.2 added

---
+++ src/DisplayFiveMessages1_2.java
@@ -0,0 +1,9 @@
+public class DisplayFiveMessages1_2 {
+    public static void main(String[] args){
+        for (int i=0; i<5; i++){
+            System.out.print("I love Java");
+            System.out.println("");
+        }
+    }
+}
+
