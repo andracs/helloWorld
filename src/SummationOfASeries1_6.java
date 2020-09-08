8e2d26d0d78dc5d2eaf6ce01908653f49a931f46
sarasandager <sara590x@edu.easj.dk>
2020-09-08 12:07:00 +0200

    Static added

--- src/SummationOfASeries1_6.java
+++ src/SummationOfASeries1_6.java
@@ -1,6 +1,6 @@
 public class SummationOfASeries1_6 {
-    public void main(String[] args){
-        System.out.print("The result of 1+2+3+4+5+6+7+8+9+10 is");
-        System.out.print(1+2+3+4+5+6+7+8+9+10);
+    public static void main(String[] args){
+        System.out.println("The result of 1+2+3+4+5+6+7+8+9+10 is");
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
     }
 }
