531b4eef7a0c57df445c740d3f8817237f3f7337
sarasandager <sara590x@edu.easj.dk>
2020-09-08 11:54:25 +0200

    Assignment 1.6 added

--- src/SummationOfASeries1_6.java
+++ src/SummationOfASeries1_6.java
@@ -1,6 +1,6 @@
 public class SummationOfASeries1_6 {
     public void main(String[] args){
         System.out.print("The result of 1+2+3+4+5+6+7+8+9+10 is");
-        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        System.out.print(1+2+3+4+5+6+7+8+9+10);
     }
 }
