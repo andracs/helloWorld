db4410aa73be48673943cc2045afdd4a3455efd4
sarasandager <sara590x@edu.easj.dk>
2020-09-08 11:22:58 +0200

    Deleted comments

--- src/ComputeExpressions1_5.java
+++ src/ComputeExpressions1_5.java
@@ -1,8 +1,5 @@
 public class ComputeExpressions1_5 {
     public static void main(String[] args){
-//        System.out.println("(7.5*6.5-4.5*3)/(47.5-5.5)");
-//        System.out.println(((7.5*6.5)-(4.5*3))/(47.5-5.5));
-
         System.out.println("Result of 7.5*6.5-4.5*3/47.5-5.5 is");
         System.out.println(7.5*6.5-4.5*3/47.5-5.5);
     }
