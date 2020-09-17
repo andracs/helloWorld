22c64e32781b9dc441d986a21d409ce47e052346
sarasandager <sara590x@edu.easj.dk>
2020-09-17 13:18:47 +0200

    Refactoring names

--- src/chapter2/ComputeLoan.java
+++ src/chapter2/ComputeLoan.java
@@ -5,45 +5,44 @@ import java.util.Scanner;
 public class ComputeLoan {
     public static void main(String[] args) {
 
-        // Create a Scanner
+        // Lav en Scanner
         Scanner input = new Scanner(System.in);
 
-        // Enter yearly interest rate
-        System.out.print("Enter yearly interest rate, for example 8.25: ");
-        double annualInterestRate = input.nextDouble();
+        // Skriv en årlig rente
+        System.out.print("Skriv en årlig rente, fx 8,25: ");
+        double aarligRente = input.nextDouble();
 
-        // Obtain monthly interest rate
-        double monthlyInterestRate = annualInterestRate / 1200;
+        // Få månedlig rente
+        double maanedligRente = aarligRente / 1200;
 
-        // Enter number of years
+        // Skriv Løbetid på lånet i år:
         System.out.print(
-                "Enter number of years as an integer, for example 5: ");
-        int numberOfYears = input.nextInt();
+                "Skriv hvor mange år du ønsker, at dit lån skal løbe over, fx 5: ");
+        int antalAarLoebetid = input.nextInt();
 
-        // Enter loan amount
-        System.out.print("Enter loan amount, for example 120000.95: ");
-        double loanAmount = input.nextDouble();
+        // Skriv lånebeløbet
+        System.out.print("Skriv det beløb, som du ønsker at låne, fx 120000,95: ");
+        double laaneBeloeb = input.nextDouble();
 
-        double monthlyPaymentResult = udregnLaanMaanedligAfdrag(loanAmount, monthlyInterestRate, numberOfYears);
-        double totalPaymentResult = udregnLaanAArligAfdrag(monthlyPaymentResult, numberOfYears);
+        double maanedligAfdragResultat = udregnLaanMaanedligAfdrag(laaneBeloeb, maanedligRente, antalAarLoebetid);
+        double totalUdbetalingsbeloebResultat = udregnLaanAArligAfdrag(maanedligAfdragResultat, antalAarLoebetid);
 
 
-        // Display results
-        System.out.println("The monthly payment is $" +
-                (int)(monthlyPaymentResult * 100) / 100.0);
-        System.out.println("The total payment is $" +
-                (int)(totalPaymentResult * 100) / 100.0);
+        // Vis resultatet
+        System.out.println("Månedlig afdrag er $" +
+                (int)(maanedligAfdragResultat * 100) / 100.0);
+        System.out.println("Totale afdrag er $" +
+                (int)(totalUdbetalingsbeloebResultat * 100) / 100.0);
     }
 
-    public static double udregnLaanMaanedligAfdrag(double loanAmount, double monthlyInterestRate, int numberOfYears) {
+    public static double udregnLaanMaanedligAfdrag(double laaneBeloeb, double maanedligRente, int antalAarLoebetid) {
         // Calculate payment
-        double monthlyPayment = loanAmount * monthlyInterestRate /(1
-                - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears *12));
-        return  monthlyPayment;
+        double maanedligAfdrag = laaneBeloeb * maanedligRente /(1
+                - 1 / Math.pow(1 + maanedligRente, antalAarLoebetid *12));
+        return  maanedligAfdrag;
     }
-    public static double udregnLaanAArligAfdrag(double monthlyPayment, int numberOfYears){
-        double totalPayment = monthlyPayment * numberOfYears * 12;
-        return totalPayment;
+    public static double udregnLaanAArligAfdrag(double maanedligAfdrag, int antalAarLoebetid){
+        double totalAfdrag = maanedligAfdrag * antalAarLoebetid * 12;
+        return totalAfdrag;
     }
-
 }
