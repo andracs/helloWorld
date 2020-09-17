ca57987cebfed0621478749880f391dd04d85b1d
sarasandager <sara590x@edu.easj.dk>
2020-09-17 13:43:30 +0200

    Refactoring names

--- src/chapter2/ComputeChange.java
+++ src/chapter2/ComputeChange.java
@@ -4,48 +4,48 @@ import java.util.Scanner;
 public class ComputeChange {
     public static void main(String[] args) {
 
-        calculateChange();
+        udregnByttepenge();
 
     }
-    public static void calculateChange(){
+    public static void udregnByttepenge(){
 
         // Create a Scanner
         Scanner input = new Scanner(System.in);
 
-        // Receive the amount
+        // Modtag beløbet
         System.out.print(
-                "Enter an amount in double, for example 11.56: ");
-        double amount = input.nextDouble();
+                "Skriv i double beløbet, fx 11,56: ");
+        double beloeb = input.nextDouble();
 
-        int remainingAmount = (int)(amount * 100);
+        int tilbagevaerendeBeloeb = (int)(beloeb * 100);
 
         // Find the number of one dollars
-        int numberOfOneDollars = remainingAmount / 100;
-        remainingAmount = remainingAmount % 100;
+        int antalEnDollars = tilbagevaerendeBeloeb / 100;
+        tilbagevaerendeBeloeb = tilbagevaerendeBeloeb % 100;
 
-        // Find the number of quarters in the remaining amount
-        int numberOfQuarters = remainingAmount / 25;
-        remainingAmount = remainingAmount % 25;
+        // Find the number of quarters in the remaining beloeb
+        int antalOneQuarters = tilbagevaerendeBeloeb / 25;
+        tilbagevaerendeBeloeb = tilbagevaerendeBeloeb % 25;
 
-        // Find the number of dimes in the remaining amount
-        int numberOfDimes = remainingAmount / 10;
-        remainingAmount = remainingAmount % 10;
+        // Find the number of dimes in the remaining beloeb
+        int antalAfDimes = tilbagevaerendeBeloeb / 10;
+        tilbagevaerendeBeloeb = tilbagevaerendeBeloeb % 10;
 
-        // Find the number of nickels in the remaining amount
-        int numberOfNickels = remainingAmount / 5;
-        remainingAmount = remainingAmount % 5;
+        // Find the number of nickels in the remaining beloeb
+        int antalAfNickels = tilbagevaerendeBeloeb / 5;
+        tilbagevaerendeBeloeb = tilbagevaerendeBeloeb % 5;
 
-        // Find the number of pennies in the remaining amount
-        int numberOfPennies = remainingAmount;
+        // Find the number of pennies in the remaining beloeb
+        int antalAfPennies = tilbagevaerendeBeloeb;
 
 
         // Display results
-        System.out.println("Your amount " + amount + " consists of");
-        System.out.println("    " + numberOfOneDollars + " dollars");
-        System.out.println("    " + numberOfQuarters + " quarters ");
-        System.out.println("    " + numberOfDimes + " dimes");
-        System.out.println("    " + numberOfNickels + " nickels");
-        System.out.println("    " + numberOfPennies + " pennies");
+        System.out.println("Dit beløb " + beloeb + " består af");
+        System.out.println("    " + antalEnDollars + " dollars");
+        System.out.println("    " + antalOneQuarters + " quarters ");
+        System.out.println("    " + antalAfDimes + " dimes");
+        System.out.println("    " + antalAfNickels + " nickels");
+        System.out.println("    " + antalAfPennies + " pennies");
 
     }
 }
